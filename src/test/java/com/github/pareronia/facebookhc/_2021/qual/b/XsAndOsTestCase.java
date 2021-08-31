42262f2ee2a23b875d1723ae932e3e2c3a368f0c
pareronia <49491686+pareronia@users.noreply.github.com>
2021-08-31 14:58:44 +0200

    2021 Qualification Round Problem B - test

---
+++ src/test/java/com/github/pareronia/facebookhc/_2021/qual/b/XsAndOsTestCase.java
@@ -0,0 +1,10 @@
+package com.github.pareronia.facebookhc._2021.qual.b;
+
+import com.github.pareronia.facebookhc.TestBase;
+
+class XsAndOsTestCase extends TestBase<XsAndOs> {
+
+    protected XsAndOsTestCase() {
+        super(XsAndOs.class);
+    }
+}
